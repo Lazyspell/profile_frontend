b81ad134f84e7d43b5edce63df847e5d1c80c53a
lazyspell <Jeremy2975@yahoo.com>
2022-07-04 22:22:29 -0700

    added logic that will contain the graphql query plus obtaining the value from the graphql

--- profile_frontend/src/routes/home/home.component.jsx
+++ profile_frontend/src/routes/home/home.component.jsx
@@ -1,5 +1,37 @@
+import { gql, useQuery } from "@apollo/client";
+import { Graphql_Query } from "../../client/profileQuerys";
+
+let testValue = "jeremy498081";
+const testQuery = Graphql_Query(testValue);
+const TEST_QUERY = gql`
+    {
+        profileId(id: "jeremy498081") {
+            id
+            name {
+                first_name
+                last_name
+            }
+            location {
+                state
+                city
+                zip_code
+            }
+            email
+        }
+    }
+`;
+
 const Home = () => {
-    return <h1>Home Component</h1>;
+    const { data, loading, error } = useQuery(TEST_QUERY);
+
+    console.log(data);
+    if (loading) return "Loading...";
+    if (error) return <pre>{error.message}</pre>;
+    return (
+        <>
+            <h1></h1>
+        </>
+    );
 };
 
 export default Home;
