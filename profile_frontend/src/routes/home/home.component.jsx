bd9cb18dfa3762a4ffd77a7a43b1753902faea20
lazyspell <Jeremy2975@yahoo.com>
2022-07-05 22:19:16 -0700

    updated home compnent to work with the graphql data and present the first and last name of the profilec

--- profile_frontend/src/routes/home/home.component.jsx
+++ profile_frontend/src/routes/home/home.component.jsx
@@ -1,35 +1,33 @@
 import { gql, useQuery } from "@apollo/client";
-import { Graphql_Query } from "../../client/profileQuerys";
-
-let testValue = "jeremy498081";
-const testQuery = Graphql_Query(testValue);
-const TEST_QUERY = gql`
-    {
-        profileId(id: "jeremy498081") {
-            id
-            name {
-                first_name
-                last_name
-            }
-            location {
-                state
-                city
-                zip_code
-            }
-            email
-        }
-    }
-`;
+import { useEffect, useState } from "react";
+import { GET_PROFILE_BY_ID } from "../../client/profileQuerys";
 
 const Home = () => {
-    const { data, loading, error } = useQuery(TEST_QUERY);
+    const { data, loading, error } = useQuery(GET_PROFILE_BY_ID, {
+        variables: {
+            id: "jeremy498081",
+        },
+    });
 
-    console.log(data);
+    useEffect(() => {
+        if (data) {
+            const {
+                profileId: { name },
+            } = data;
+
+            setFullName(name);
+        }
+    }, [data]);
+
+    const [fullName, setFullName] = useState({});
+
     if (loading) return "Loading...";
     if (error) return <pre>{error.message}</pre>;
     return (
         <>
-            <h1></h1>
+            <h1>
+                {fullName.first_name} {fullName.last_name}
+            </h1>
         </>
     );
 };
