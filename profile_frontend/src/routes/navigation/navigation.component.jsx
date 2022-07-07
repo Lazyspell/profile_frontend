e898ede49a8bf9a8392c5ba53c358f9d681cb3e2
lazyspell <Jeremy2975@yahoo.com>
2022-07-06 23:50:07 -0700

    added basic functionality and looks of the nav bar

--- profile_frontend/src/routes/navigation/navigation.component.jsx
+++ profile_frontend/src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,69 @@
+import { Navbar, Card, ListGroup, ListGroupItem } from "react-bootstrap";
+
+import "./navigation.css";
+
 const Navigation = () => {
-    return <h1>Navigation Bar</h1>;
+    return (
+        <>
+            <Navbar className="home">
+                <h1>Jeremy Elam</h1>
+                <Card className="card">
+                    <Card.Img
+                        // src ={}
+                        variant="top"
+                        className="sig"
+                    />
+                    <Card.Body>
+                        <Card.Text>
+                            <ListGroup variant="flush">
+                                <ListGroupItem className="list">
+                                    <span
+                                        className="link"
+                                        onClick={() => {
+                                            console.log("home");
+                                        }}
+                                    >
+                                        Home
+                                    </span>
+                                </ListGroupItem>
+
+                                <ListGroupItem className="list">
+                                    <span
+                                        className="link"
+                                        onClick={() => {
+                                            console.log("About");
+                                        }}
+                                    >
+                                        About
+                                    </span>
+                                </ListGroupItem>
+                                <ListGroupItem className="list">
+                                    <span
+                                        className="link"
+                                        onClick={() => {
+                                            console.log("Portfolio");
+                                        }}
+                                    >
+                                        Portfolio
+                                    </span>
+                                </ListGroupItem>
+                                <ListGroupItem className="list">
+                                    <span
+                                        className="link"
+                                        onClick={() => {
+                                            console.log("Contact");
+                                        }}
+                                    >
+                                        Contact
+                                    </span>
+                                </ListGroupItem>
+                            </ListGroup>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Navbar>
+        </>
+    );
 };
 
 export default Navigation;
