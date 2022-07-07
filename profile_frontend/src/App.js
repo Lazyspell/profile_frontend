6107684328bcab8e4db10d85b686bdc25b6d9ba6
lazyspell <Jeremy2975@yahoo.com>
2022-07-06 23:48:39 -0700

    uncommoneted some code for testing

--- profile_frontend/src/App.js
+++ profile_frontend/src/App.js
@@ -6,9 +6,9 @@ import Home from "./routes/home/home.component";
 function App() {
     return (
         <Routes>
-            {/* <Route path="/" element={<Navigation />}> */}
-            <Route index element={<Home />} />
-            {/* </Route> */}
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />} />
+            </Route>
         </Routes>
     );
 }
