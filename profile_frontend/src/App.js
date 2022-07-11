2cf7fb31ee356fde8ce6bb6a0074f8f33c8ee7ab
lazyspell <Jeremy2975@yahoo.com>
2022-07-10 23:05:56 -0700

    added new route to test sphere animation component

--- profile_frontend/src/App.js
+++ profile_frontend/src/App.js
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+import { Loader } from "./components/loader/loader.component";
+import { AnimatedSphere } from "./components/animation/cloudSphere.component";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Navigation />}>
                 <Route index element={<Home />} />
+                <Route path="/checkout" element={<AnimatedSphere />} />
             </Route>
         </Routes>
     );
