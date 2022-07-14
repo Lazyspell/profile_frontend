65b3197a0d3007bd090b228f676ccaf05ed88796
lazyspell <Jeremy2975@yahoo.com>
2022-07-13 21:18:57 -0700

    added about page

---
+++ profile_frontend/src/routes/about/about.component.jsx
@@ -0,0 +1,22 @@
+import "./about.styles.scss";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { AnimatedSphere } from "../../components/animation/cloudSphere/cloudSphere.component";
+export const About = () => {
+    return (
+        <>
+            <div className="main-container">
+                <div class="split left">
+                    <div class="centered"></div>
+                </div>
+
+                <div class="split right">
+                    <div class="centered">
+                        <AnimatedSphere />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
