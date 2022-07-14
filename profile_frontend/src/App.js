cd7461ff20eaa8c199d8897413f63bd30b84bbe6
lazyspell <Jeremy2975@yahoo.com>
2022-07-13 21:16:22 -0700

    added about page to the routes

--- profile_frontend/src/App.js
+++ profile_frontend/src/App.js
@@ -4,13 +4,14 @@ import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import { Loader } from "./components/loader/loader.component";
 import { AnimatedSphere } from "./components/animation/cloudSphere/cloudSphere.component";
+import { About } from "./routes/about/about.component";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Navigation />}>
                 <Route index element={<Home />} />
-                <Route path="/animation" element={<AnimatedSphere />} />
+                <Route path="/about" element={<About />} />
             </Route>
         </Routes>
     );
