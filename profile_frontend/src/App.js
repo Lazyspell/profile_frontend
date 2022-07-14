f1b3dcee74d96d20f9f958e41d631814c2da0e2b
lazyspell <Jeremy2975@yahoo.com>
2022-07-13 23:08:32 -0700

    added new links to the nav bar to go to skills work and contact page

--- profile_frontend/src/App.js
+++ profile_frontend/src/App.js
@@ -5,6 +5,9 @@ import Home from "./routes/home/home.component";
 import { Loader } from "./components/loader/loader.component";
 import { AnimatedSphere } from "./components/animation/cloudSphere/cloudSphere.component";
 import { About } from "./routes/about/about.component";
+import { Skills } from "./routes/skills/skills.component";
+import { Work } from "./routes/work/work.component";
+import { Contact } from "./routes/contact/contact.component";
 
 function App() {
     return (
@@ -12,6 +15,9 @@ function App() {
             <Route path="/" element={<Navigation />}>
                 <Route index element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/work" element={<Work />} />
+                <Route path="/contact" element={<Contact />} />
             </Route>
         </Routes>
     );
