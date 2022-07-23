b9d2504198180a59513de9ac919e1c6722c94568
lazyspell <Jeremy2975@yahoo.com>
2022-07-22 21:11:55 -0700

    new graphql query to get new profile data

--- profile_frontend/src/client/profileQuery.js
+++ profile_frontend/src/client/profileQuery.js
@@ -1,20 +1,49 @@
 import gql from "graphql-tag";
 
 export const GET_PROFILE_BY_ID = gql`
-    query findProfiles($id: ID!) {
-        profileId(id: $id) {
-            id
-            name {
-                first_name
-                last_name
+    query findProfiles($email: String!) {
+        profileId(email: $email) {
+            first_name
+            last_name
+            dob {
+                day
+                month
+                year
+                age
             }
             location {
                 state
                 city
                 zip_code
             }
-            email
-            description
+            skills {
+                tech_name
+                tech_link
+                years_of_experience
+                tech_description
+            }
+            projects {
+                project_name
+                frontend_link
+                frontend_description
+                backend_link
+                backend_description
+            }
+            contact {
+                phone_number
+                email
+                github
+                linkedin
+                discord
+                about_me
+                about_my_career
+            }
+            experience {
+                company_name
+                years_worked
+                work_description
+                tech_used
+            }
         }
     }
 `;
