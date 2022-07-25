aa2c82a987ac41d7e4e14909acb6b427d0f16a03
lazyspell <Jeremy2975@yahoo.com>
2022-07-25 14:14:36 -0700

    added context about_my_career variable to webpage

--- profile_frontend/src/routes/about/about.component.jsx
+++ profile_frontend/src/routes/about/about.component.jsx
@@ -3,17 +3,23 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { AnimatedSphere } from "../../components/animation/cloudSphere/cloudSphere.component";
+import { useContext } from "react";
+import { ProfileContext } from "../../contexts/profile.context";
+
 export const About = () => {
+    const { contact } = useContext(ProfileContext);
+    const { about_my_career } = contact;
     return (
         <>
             <div className="main-container">
                 <div className="split left">
                     <div className="adjust-center">
-                        <h1 className="text-header">Me, Myself, & I</h1>
-                        <br />
-                        <p className="text-message">
-                            Messages \n Another message
-                        </p>
+                        <div className="about-header">
+                            <h1 className="text-header">Me, Myself, & I</h1>
+                        </div>
+                        <div className="about-message">
+                            <p className="text-message">{about_my_career}</p>
+                        </div>
                     </div>
                 </div>
 
