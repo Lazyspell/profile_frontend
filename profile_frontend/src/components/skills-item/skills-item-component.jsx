cfe02e0e33550b25330bb5048252f593242bfe6b
lazyspell <Jeremy2975@yahoo.com>
2022-07-26 22:53:26 -0700

    all the skill items that will be added to the page will be styled here

---
+++ profile_frontend/src/components/skills-item/skills-item-component.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import "./skills-item.styles.scss";
+
+export const SkillItem = ({ inputSkills }) => {
+    const {
+        tech_name,
+        tech_link,
+        years_of_experience,
+        tech_description,
+        image_link,
+    } = inputSkills;
+    console.log(inputSkills);
+    return (
+        <div className="skill-item-container">
+            <div
+                className="background-image"
+                style={{
+                    backgroundImage: `url(${image_link})`,
+                }}
+            />
+            <div className="skills-body">
+                <h2>{tech_name}</h2>
+                <p> {tech_description}</p>
+                <span>Years of Experience: {years_of_experience}</span>
+            </div>
+        </div>
+    );
+};
