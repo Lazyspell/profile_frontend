231a3d059fc74a052997db1a36057ba8f4ec7be1
Jeremy Elam <lazyspell@Jeremys-MacBook-Pro.local>
2022-08-01 11:25:44 -0700

    update to have correct images

--- profile_frontend/src/components/skills-item/skills-item-component.jsx
+++ profile_frontend/src/components/skills-item/skills-item-component.jsx
@@ -1,19 +1,20 @@
 import "./skills-item.styles.scss";
 
 export const SkillItem = ({ inputSkills }) => {
-    const { tech_name, years_of_experience, tech_description } = inputSkills;
+    const { tech_name, years_of_experience, tech_description, image_url } =
+        inputSkills;
     console.log(inputSkills);
     return (
         <div className="skill-item-container">
             <div
                 className="background-image"
                 style={{
-                    backgroundImage: `url(${"https://www.freepnglogos.com/uploads/javascript-png/javascript-nodejs-logo-27.png"})`,
+                    backgroundImage: `url(${image_url})`,
                 }}
             />
             <div className="skills-body">
                 <h2>{tech_name}</h2>
-                <p> {tech_description}</p>
+                <div> {tech_description}</div>
                 <span>Years of Experience: {years_of_experience}</span>
             </div>
         </div>
