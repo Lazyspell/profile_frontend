5bca0c03aa4038386f25838ebd8956cd1f70bd46
lazyspell <jeremy2975@yahoo.com>
2022-08-03 13:54:25 -0700

    creates material UI cards for my info

--- profile_frontend/src/components/skills-item/skills-item-component.jsx
+++ profile_frontend/src/components/skills-item/skills-item-component.jsx
@@ -1,22 +1,55 @@
 import "./skills-item.styles.scss";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+import { Button } from "../../components/button/button.component";
 
 export const SkillItem = ({ inputSkills }) => {
     const { tech_name, years_of_experience, tech_description, image_url } =
         inputSkills;
     console.log(inputSkills);
     return (
-        <div className="skill-item-container">
-            <div
-                className="background-image"
-                style={{
-                    backgroundImage: `url(${image_url})`,
-                }}
+        <Card sx={{ maxWidth: 300 }} style={{ backgroundColor: "black" }}>
+            <CardMedia
+                component="img"
+                height="140"
+                image={image_url}
+                alt={tech_name}
+                style={{ backgroundColor: "black" }}
             />
-            <div className="skills-body">
-                <h2>{tech_name}</h2>
-                <p> {tech_description}</p>
-                <span>Years of Experience: {years_of_experience}</span>
-            </div>
-        </div>
+            <CardContent>
+                <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    style={{ backgroundColor: "black" }}
+                    color="white"
+                >
+                    {tech_name}
+                </Typography>
+                <Typography
+                    variant="body2"
+                    color="white"
+                    style={{ backgroundColor: "black" }}
+                >
+                    {tech_description}
+                </Typography>
+                <br></br>
+                <Typography
+                    variant="body2"
+                    color="white"
+                    style={{ backgroundColor: "black" }}
+                    className="test"
+                >
+                    Years of Experience: {years_of_experience}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button>Learn More</Button>
+            </CardActions>
+        </Card>
     );
 };
