3cf4352ac7eb75fab903eee0b5f48cce7f8e8368
lazyspell <jeremy2975@yahoo.com>
2022-08-04 21:58:43 -0700

    added carousel to file with css changes

--- profile_frontend/src/components/skills-container/skills-container.component.jsx
+++ profile_frontend/src/components/skills-container/skills-container.component.jsx
@@ -1,12 +1,14 @@
 import { SkillItem } from "../skills-item/skills-item.component.jsx";
+import Carousel from "react-material-ui-carousel";
+
 import "./skills-container.styles.scss";
 
 export const SkillsContainer = ({ skills }) => {
     return (
-        <div className="skill-container">
+        <Carousel>
             {skills.map((skill, index) => (
                 <SkillItem key={index + 1} inputSkills={skill} />
             ))}
-        </div>
+        </Carousel>
     );
 };
