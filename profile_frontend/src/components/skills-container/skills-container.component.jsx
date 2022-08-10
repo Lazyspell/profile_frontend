5f36453d0012d4818dd767780acb42eb6bc8b8b6
lazyspell <jeremy2975@yahoo.com>
2022-08-09 22:17:50 -0700

    update to work with new schema

--- profile_frontend/src/components/skills-container/skills-container.component.jsx
+++ profile_frontend/src/components/skills-container/skills-container.component.jsx
@@ -4,11 +4,33 @@ import Carousel from "react-material-ui-carousel";
 import "./skills-container.styles.scss";
 
 export const SkillsContainer = ({ skills }) => {
+    const { frontend, backend, machine_learning, cloud_service } = skills;
     return (
         <>
-            <h1>Test</h1>
+            <h1>Backend</h1>
             <Carousel className="skill-container">
-                {skills.map((skill, index) => (
+                {backend.map((skill, index) => (
+                    <SkillItem key={index + 1} inputSkills={skill} />
+                ))}
+            </Carousel>
+
+            <h1>Frontend</h1>
+            <Carousel className="skill-container">
+                {frontend.map((skill, index) => (
+                    <SkillItem key={index + 1} inputSkills={skill} />
+                ))}
+            </Carousel>
+
+            <h1>Machine Learning</h1>
+            <Carousel className="skill-container">
+                {machine_learning.map((skill, index) => (
+                    <SkillItem key={index + 1} inputSkills={skill} />
+                ))}
+            </Carousel>
+
+            <h1>Cloud Service</h1>
+            <Carousel className="skill-container">
+                {cloud_service.map((skill, index) => (
                     <SkillItem key={index + 1} inputSkills={skill} />
                 ))}
             </Carousel>
