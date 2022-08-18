91db629f5e9c9146e35aa593796d738a5070975b
lazyspell <jeremy2975@yahoo.com>
2022-08-17 20:34:23 -0700

    added google map to contact page

--- profile_frontend/src/routes/contact/contact.component.jsx
+++ profile_frontend/src/routes/contact/contact.component.jsx
@@ -1,9 +1,16 @@
+import MyComponent from "../../components/google-maps/google-maps.component";
 import "./contact.styles.scss";
-
 export const Contact = () => {
+    console.log(process.env);
+    console.log("test");
     return (
         <>
-            <div className="contact-container">Contact Page</div>
+            <div className="contact-container">
+                Contact Page
+                <div>
+                    <MyComponent />
+                </div>
+            </div>
         </>
     );
 };
