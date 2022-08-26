21930e9fc8346dbbc1cd0212675bd893f257d678
lazyspell <jeremy2975@yahoo.com>
2022-08-25 23:30:10 -0700

    added workcontainer to left side webpage

--- profile_frontend/src/routes/work/work.component.jsx
+++ profile_frontend/src/routes/work/work.component.jsx
@@ -1,10 +1,17 @@
+import { WorkContainer } from "../../components/work-container/work-container.styles";
+import { ProfileContext } from "../../contexts/profile.context";
+import { useContext } from "react";
+
 import "./work.styles.scss";
 
 export const Work = () => {
+    const { experience } = useContext(ProfileContext);
     return (
         <>
             <div className="work-container">
-                <div className="work-left"></div>
+                <div className="work-left">
+                    <WorkContainer experiences={experience} />
+                </div>
                 <div className="work-right"></div>
             </div>
         </>
