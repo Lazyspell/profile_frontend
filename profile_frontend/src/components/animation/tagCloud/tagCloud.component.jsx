190572f782be5782e4cde1bcf649d116a1790b31
lazyspell <jeremy2975@yahoo.com>
2022-09-03 16:38:17 -0700

    added tagCloud component probably won't be used

--- profile_frontend/src/components/animation/tagCloud/tagCloud.component.jsx
+++ profile_frontend/src/components/animation/tagCloud/tagCloud.component.jsx
@@ -1,26 +1,9 @@
-import { useContext } from "react";
 import TagCloud from "react-tag-cloud";
 import randomColor from "randomcolor";
 
-import { ProfileContext } from "../../../contexts/profile.context";
-
-export const TagCloudAnimation = () => {
-    const { experience } = useContext(ProfileContext);
-
-    const tagCloudArray = [];
-    const experienceArray = experience[0].tech_used;
-
-    for (let i of experienceArray) {
-        const newObject = {
-            value: i,
-            count: Math.floor(Math.random() * 40) + 30,
-        };
-
-        tagCloudArray.push(newObject);
-    }
-
+export const TagCloudAnimation = ({ data }) => {
     return (
-        <>
+        <div className="tag-cloud">
             <TagCloud
                 style={{
                     fontFamily: "sans-serif",
@@ -29,14 +12,12 @@ export const TagCloudAnimation = () => {
                     fontStyle: "italic",
                     color: () => randomColor(),
                     padding: 5,
-                    width: "100%",
-                    height: "100%",
                 }}
             >
-                <div>react</div>
-                <div>tag</div>
-                <div>cloud</div>
+                {data.map((item, index) => (
+                    <div key={index + 1}>{item}</div>
+                ))}
             </TagCloud>
-        </>
+        </div>
     );
 };
